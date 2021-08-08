817b989794de46372ac7d4b3d5c50ba5b83e3cc3
BrendaSpencer <spencerbernadette1@gmail.com>
2021-08-08 19:22:46 +0200

    index

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+//index.php
+require_once('header.php');
+
+?>
+
+<h1>Index Pagina</h1>
+<p>Welkom op deze kleine website rond gebruikerstoegang.</p>
+
+<?php
+require_once('footer.php');
+?>
