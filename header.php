26d1e7a2290e5f6f7461dd835803ff4b01cc0f78
BrendaSpencer <spencerbernadette1@gmail.com>
2021-08-08 19:17:14 +0200

    header_footer

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--header.php-->
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gebruikers toegang</title>
+</head>
+<body>
+    <a href="index.php">Home</a> -
+    <a href="publicpage.php">Public PAge</a> -
+    <a href="login.php"> Login</a> -
+    <a href="register.php">Registreren</a> -
+    <a href="privatepage.php"> Private Page</a> - 
+    <a href="Logout.php">Logout</a>
